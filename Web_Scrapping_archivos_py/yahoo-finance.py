3e6b277e6f74691f06eb25903162aaa99e9d9312
GonEzq <gonzaloeriveral@gmail.com>
2023-11-05 12:28:40 -0300

    se actualiza

--- Web_Scrapping_archivos_py/yahoo-finance.py
+++ Web_Scrapping_archivos_py/yahoo-finance.py
@@ -2,11 +2,11 @@ import yfinance as yf      # usando la librería yfinance
 import pandas as pd        # librería pandas
 
 
-ticker = "APPL"  # símbolo del título bursátil en website: https://finance.yahoo.com/
+ticker = "CL=F"  # símbolo del título bursátil en website: https://finance.yahoo.com/
 # nombre del título bursátil
-nombre_ticker = "Apple_histórico"
-data_start = "2022-01-01"  # fecha de inicio de la serie
-data_end = "2023-01-01"    # fecha de cierre de la serie
+nombre_ticker = " Petróleo Crudo"
+data_start = "2023-01-01"  # fecha de inicio de la serie
+data_end = "2023-09-30"    # fecha de cierre de la serie
 
 # Se descarga la serie con el siguiente método de la librería yfinance.
 
